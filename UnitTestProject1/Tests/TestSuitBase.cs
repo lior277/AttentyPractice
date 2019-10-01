32630e5ebf463157056e65c2b6b21a6df97604fe
lior277 <lior277@gmail.com>
2019-10-01 17:21:16 +0300

    yu

---
+++ UnitTestProject1/Tests/TestSuitBase.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Net.Http;
+
+namespace Tests
+{
+    public class TestSuitBase
+    {
+        private static string driverDir = Environment.CurrentDirectory;
+
+        public string convectorSiteUrl = "https://www.metric-conversions.org/";
+        public string weatherSiteUrl = "https://weather.com/";
+
+        private IWebDriver m_WebDriver;
+        public virtual IWebDriver WebDriver { get => m_WebDriver ?? 
+                (m_WebDriver = new ChromeDriver(driverDir)); private set => m_WebDriver = value; }
+  
+        public void Dispose()
+        {
+            if (m_WebDriver != null)
+            {
+                WebDriver.Dispose();
+                WebDriver = null;
+            }          
+        }
+    }
+}
